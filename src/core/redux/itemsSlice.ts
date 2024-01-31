65b7b1c2357bc14972da659467c5928805b97909
tschanomis <tschan.omis@gmail.com>
2024-01-31 17:18:06 +0100

    config redux

---
+++ src/core/redux/itemsSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ItemsState {
+  value: Array<string>;
+}
+
+const itemsString = localStorage.getItem("appItems");
+const items = itemsString ? JSON.parse(itemsString) : [];
+
+const initialState: ItemsState = {
+  value: items,
+};
+
+export const itemsSlice = createSlice({
+  name: "items",
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+  },
+});
+
+export const { addItem } = itemsSlice.actions;
+
+export default itemsSlice.reducer;
